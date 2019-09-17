9a01612573308c13c448ed2e2707b5273d5eeb76
Julio Cesar Rodriguez Rodriguez <julio.rodriguezr1@gmail.com>
2019-09-17 16:52:36 -0300

    this is the first commit

---
+++ src/app/shared/amchartDatum.ts
@@ -0,0 +1,43 @@
+export const datum1 = [{
+    "year": 2005,
+    "income": 23.5,
+    "expenses": 18.1
+}, {
+    "year": 2006,
+    "income": 26.2,
+    "expenses": 22.8
+}, {
+    "year": 2007,
+    "income": 30.1,
+    "expenses": 23.9
+}, {
+    "year": 2008,
+    "income": 29.5,
+    "expenses": 25.1
+}, {
+    "year": 2009,
+    "income": 24.6,
+    "expenses": 24.6
+}];
+
+export const datum2 = [{
+    "year": 2005,
+    "income": 33.5,
+    "expenses": 38.1
+}, {
+    "year": 2006,
+    "income": 26.2,
+    "expenses": 22.8
+}, {
+    "year": 2007,
+    "income": 24.1,
+    "expenses": 17.9
+}, {
+    "year": 2008,
+    "income": 23.5,
+    "expenses": 27.1
+}, {
+    "year": 2009,
+    "income": 28.6,
+    "expenses": 24.3
+}];
